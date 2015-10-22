f7465dd62853ade38623a347f3e46399e0abf3f1
Alberto <alberto.palazzin@gmail.com>
2015-10-22 11:15:54 +0200

    Support for users that don't have JavaScript enabled  + new default error message

--- base-class.php
+++ base-class.php
@@ -66,6 +66,31 @@ class WC_Ncr_No_Captcha_Recaptcha {
 	public static function display_captcha() {
 
 		echo '<div class="g-recaptcha" data-sitekey="' . self::$site_key . '" data-theme="' . self::$theme . '"></div>';
+
+		// Support for users that don't have JavaScript enabled
+		// https://developers.google.com/recaptcha/docs/faq
+		echo '
+		<noscript>
+		  <div style="width: 302px; height: 422px;">
+		    <div style="width: 302px; height: 422px; position: relative;">
+		      <div style="width: 302px; height: 422px; position: absolute;">
+		        <iframe src="https://www.google.com/recaptcha/api/fallback?k=' . self::$site_key . '"
+		                frameborder="0" scrolling="no"
+		                style="width: 302px; height:422px; border-style: none;">
+		        </iframe>
+		      </div>
+		      <div style="width: 300px; height: 60px; border-style: none;
+		                  bottom: 12px; left: 25px; margin: 0px; padding: 0px; right: 25px;
+		                  background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px;">
+		        <textarea id="g-recaptcha-response" name="g-recaptcha-response"
+		                  class="g-recaptcha-response"
+		                  style="width: 250px; height: 40px; border: 1px solid #c1c1c1;
+		                         margin: 10px 25px; padding: 0px; resize: none;" >
+		        </textarea>
+		      </div>
+		    </div>
+		  </div>
+		</noscript>';
 	}
 
 	/**
@@ -104,7 +129,7 @@ class WC_Ncr_No_Captcha_Recaptcha {
 			'captcha_wc_registration' => 'yes',
 			'captcha_wc_comment'      => 'yes',
 			'theme'                => 'light',
-			'error_message'        => __('<strong>ERROR</strong>: Please retry CAPTCHA', 'wc-no-captcha')
+			'error_message'        => __('<strong>ERROR</strong>: Please fill in the reCAPTCHA form.', 'wc-no-captcha')
 		);
 
 		add_option( 'wc_ncr_options', $default_options );
