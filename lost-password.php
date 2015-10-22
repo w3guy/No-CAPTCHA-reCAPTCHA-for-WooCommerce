d7b9145c12735d55476adefef0b64eb8bebd28c9
Alberto <alberto.palazzin@gmail.com>
2015-10-22 16:53:22 +0200

    Fix all in WC_Ncr_Lost_Password_Captcha class

--- lost-password.php
+++ lost-password.php
@@ -7,25 +7,27 @@ class WC_Ncr_Lost_Password_Captcha extends WC_Ncr_No_Captcha_Recaptcha {
 
 	public static function initialize() {
 
-		// initialize if login is activated
+		// initialize if lost_password is activated
 		if ( isset( self::$plugin_options['captcha_wc_lost_password'] ) && self::$plugin_options['captcha_wc_lost_password'] == 'yes' ) {
-
-			// adds the captcha to the login form
-			add_filter( 'woocommerce_lostpassword_form', array( __CLASS__, 'display_captcha' ) );
-
+			// add the captcha to the lost_password form
+			// NB: do_action( 'woocommerce_lostpassword_form' ) is needed in theme form-lost-password page
+			add_action( 'woocommerce_lostpassword_form', array( __CLASS__, 'display_captcha' ) );
+			
 			// authenticate the captcha answer
-			add_action( 'allow_password_reset', array( __CLASS__, 'validate_lost_password_captcha' ) );
+			add_filter( 'allow_password_reset', array( __CLASS__, 'validate_lost_password_captcha' ) );
 		}
 	}
 
 	/**
-	 * Verify the captcha answer.
+	 * Verify the captcha answer
 	 *
-	 * @return WP_Error
+	 * @return WP_Error or boolean
 	 */
-	public static function validate_lost_password_captcha() {
+	public static function validate_lost_password_captcha( $result, $user_id ) {
 		if ( ! isset( $_POST['g-recaptcha-response'] ) || ! self::captcha_wc_verification() ) {
-			return new WP_Error( 'empty_captcha', self::$error_message );
+ 		 	$result = new WP_Error( 'empty_captcha', self::$error_message );	 	
 		}
+		
+		return $result;
 	}
 }
